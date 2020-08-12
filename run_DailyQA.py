4706ee88253edb8995088ae2b56cc4fa8d2772f3
papo <papomail@gmail.com>
2020-08-12 11:18:45 +0100

    output tweaks

--- run_DailyQA.py
+++ run_DailyQA.py
@@ -19,13 +19,14 @@ from definitions_DailyQA import *
 import sys
 
 
-def run_tests(input_folder='/Users/papo/Sync/Projects/DQA_tests/DATA'):
+def run_tests(input_folder='/Users/papo/Sync/Projects/DQA_tests/DATA/RT_Ingenia_Coil_data'):
 
     input_folder=Path(input_folder)
     nifti_folder=convert2NIFTI(input_folder)
+    print(f'nifti_folder={nifti_folder}')
     test_files=parse_files(nifti_folder)
     tests=[SNR_test(files) for files in test_files]
-
+    
 
     # %%
     # Show results and combine them into a df
@@ -34,7 +35,7 @@ def run_tests(input_folder='/Users/papo/Sync/Projects/DQA_tests/DATA'):
     plt.close('all')
     for n,test in enumerate(tests):
         print(f'test {n+1}, {test.name}:\n   SNR={int(np.round(test.SNR_global))}\n   nSNR={int(np.round(test.nSNR))}\n')
-        test.plot(out_file=f'ROI_SNR_{n+1}.png')
+        test.plot(out_file=nifti_folder/f'Test{n+1}_ROI.png')
         frames.append(test.results_df)
 
     # plt.show(block=False) 
@@ -43,13 +44,15 @@ def run_tests(input_folder='/Users/papo/Sync/Projects/DQA_tests/DATA'):
     all_df.reset_index(drop=True, inplace=True)
     all_df['Acquisition Date'] = pd.to_datetime(all_df['AcquisitionDateTime'])
     all_df.drop(columns=['AcquisitionDateTime'],inplace=True)
+    all_df.to_hdf(nifti_folder/f'Results_Summary{datetime.now().strftime("_%d%b%Y")}')
 
 
     # %%
-    # fig1=px.scatter(all_df, x='Acquisition Date', y='NSNR',error_y='NSNR_std',title=f'Daily QA: {all_df.ManufacturersModelName[0]} ({all_df.InstitutionAddress[0]})',color='Name')
-    fig2=px.bar(all_df, x='Acquisition Date', y='NSNR',error_y='NSNR_std',title=f'Daily QA: {all_df.ManufacturersModelName[0]} ({all_df.InstitutionAddress[0]})',color='Name')
-    # fig1.show()
-    fig2.show()
+    barchart=px.bar(all_df, x='Acquisition Date', y='NSNR',error_y='NSNR_std',title=f'Daily QA: {all_df.ManufacturersModelName[0]} ({all_df.InstitutionAddress[0]})',color='Name')
+    barchart.for_each_trace(lambda x: x.update(error_y_color=x.marker.color))
+    barchart.show()
 
-
-run_tests(sys.argv[1]) if sys.argv[1] else run_tests()
+try:
+    run_tests(sys.argv[1])
+except:
+    run_tests()
